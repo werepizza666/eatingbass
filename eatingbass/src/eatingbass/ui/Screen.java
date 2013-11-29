e3df93d70b802b31f3c406414da41207af630fad
werepizza666 <werepizza666@gmail.com>
2013-11-29 13:02:33 +0200

    vko 5
    renderöintiin lisätty läpinäkyvyys spriteille

--- eatingbass/src/eatingbass/ui/Screen.java
+++ eatingbass/src/eatingbass/ui/Screen.java
@@ -24,6 +24,7 @@ public class Screen {
      * päivittää peliruudun pikselit
      */
     public void render() {
+        
         for (int y = 0; y < height; y++) {
 //            
 //            if (y < 0 || y >= height) {
@@ -35,21 +36,31 @@ public class Screen {
 //                }
                 pixels[x + y * width] = Sprite.wall.pixels[(x % 20) + (y % 20) * Sprite.wall.SIZE];
                 if (y >= 160) {
-                    pixels[x + y * width] = Sprite.water.pixels[(x % 20) + (y % 20) * Sprite.water.SIZE];
+                    if (!checkIfPixelEmpty(Sprite.water, (x % 20) + (y % 20) * Sprite.water.SIZE)) {
+                        pixels[x + y * width] = Sprite.water.pixels[(x % 20) + (y % 20) * Sprite.water.SIZE];
+                    }
+//                    System.out.println(pixels[x + y * width]);
                 }
                 if (y >= 160 && Level.level1.playerLocation() <= x && x < Level.level1.playerLocation() + 20) {
-                    pixels[x + y * width] = Sprite.player.pixels[(x % 20) + (y % 20) * Sprite.player.SIZE];
+                    if (!checkIfPixelEmpty(Sprite.player, ((x % 20) + (y % 20) * Sprite.player.SIZE))) {
+                        pixels[x + y * width] = Sprite.player.pixels[(x % 20) + (y % 20) * Sprite.player.SIZE];
+                    }
                 }
                 for (Fish f : Level.level1.getFishList()) {
+
                     if ((f.getY() * 20) <= y && y < ((f.getY() * 20) + 20) && (f.getX() * 20) <= x && x < ((f.getX() * 20) + 20)) {
-//                        if ((Sprite.fish.pixels[(x & 20) + (y % 20) * Sprite.fish.SIZE)]) != 0) {
-                        pixels[x + y * width] = Sprite.fish.pixels[(x % 20) + (y % 20) * Sprite.fish.SIZE];
-//                        }
+                        if (!checkIfPixelEmpty(Sprite.fish, ((x % 20) + (y % 20) * Sprite.fish.SIZE))) {
+
+                            pixels[x + y * width] = Sprite.fish.pixels[(x % 20) + (y % 20) * Sprite.fish.SIZE];
+                        }
                     }
                 }
                 for (Rock r : Level.level1.getRockList()) {
                     if ((r.getY() * 20) <= y && y < ((r.getY() * 20) + 20) && (r.getX() * 20) <= x && x < ((r.getX() * 20) + 20)) {
-                        pixels[x + y * width] = Sprite.rock.pixels[(x % 20) + (y % 20) * Sprite.rock.SIZE];
+                        if (!checkIfPixelEmpty(Sprite.rock, ((x % 20) + (y % 20) * Sprite.rock.SIZE))) {
+
+                            pixels[x + y * width] = Sprite.rock.pixels[(x % 20) + (y % 20) * Sprite.rock.SIZE];
+                        }
                     }
                 }
             }
@@ -73,5 +84,12 @@ public class Screen {
      */
     public int getPixel(int i) {
         return pixels[i];
+    }
+
+    public boolean checkIfPixelEmpty(Sprite s, int i) {
+        if (s.pixels[i] == -65281) {
+            return true;
+        }
+        return false;
     }
 }
